e55ab593bdacc1f2512bef6db4e5346f5ad5d6c3
david-cl <49204627+david-cl@users.noreply.github.com>
2019-04-02 18:34:55 +0200

    Add files via upload

---
+++ ManejoDeCadenas.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejo.de.cadenas;
+
+/**
+ *
+ * @author PC04-LAB02
+ */
+public class ManejoDeCadenas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nm="david";
+        String ap="choque";
+        String Concatenacion=(nm+ap);
+        System.out.println("su nombres es:"+Concatenacion);
+        System.out.println(" nueva linea: \n"+ nm +" "+ ap);
+        System.out.println(" tabulador: \n"+ nm +"\t "+ ap);
+        System.out.println(" retroseso: \n"+ nm +" \b"+ ap);
+        System.out.println(" retorno de carro : \n"+ nm +"\r "+ ap);
+        System.out.println(" comilla simple: \n" + nm +" "+ ap +"\n");
+        System.out.println(" comilla doble: \n"+ nm +" "+ap + "\n");
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ VariableByte.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variable.pkgbyte;
+
+/**
+ *
+ * @author PC04-LAB02
+ */
+public class VariableByte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b1=10;
+        byte  b2=oxa;
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
