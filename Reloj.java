fdbfb23243d9a73076bf2e1fb9682ad9ed3be0bd
david <49204627+david-cl@users.noreply.github.com>
2019-04-28 23:06:41 -0500

    Add files via upload

---
+++ Reloj.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reloj;
+
+/**
+ *
+ * @author PC04-LAB02
+ */
+public class Reloj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int r=59;
+        
+       for(int c=1; c<=9; c++)
+       { for(int x=0;x<=r;x++)
+           System.out.println(c+ ":"+x);
+       }
+       }
+        
+        
+    }
+    
+}
